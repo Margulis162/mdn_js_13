0ccf15a9388061fc8afde0a7a025c7d18d1984cb
Margulis162 <markmargulis@proton.me>
2023-10-01 16:46:53 -0400

    animating with pyramid of call back hell. Well, sort of. I still have to  use  promise from the animate object since I do not really path anything throuh those callbacks. An alternative way is to use  spiral.animate(aliceTumbling, aliceTiming).onfinish = callback;

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const aliceTumbling = [
   { transform: 'rotate(0) scale(1)' },
-  { transform: 'rotate(360deg) scale(.7)' },
+  { transform: 'rotate(360deg) scale(.4)' },
   { transform: 'rotate(720deg) scale(1)' }
 ];
 
 const aliceTiming = {
-  duration: 2000,
-  iterations: Infinity,
+  duration: 1000000,
+  iterations: 10,
   fill: 'forwards'
 }
 
@@ -15,4 +15,33 @@ const alice2 = document.querySelector("#alice2");
 const alice3 = document.querySelector("#alice3");
 
 
-alice1.animate(aliceTumbling, aliceTiming,);
+
+
+
+// here will be my code
+
+// animating with pyramyd of dooom
+
+function animate_spiral(spiral, callback,){
+   let x = spiral.animate(aliceTumbling, aliceTiming,);
+   x.finished.then(() => {
+    callback();
+   }
+    
+   )
+   
+}
+
+animate_spiral( alice1, () => {
+  animate_spiral(alice2, () => {
+    animate_spiral(alice3,() => {
+      console.log("sucsess");
+    })
+  })
+})
+// animating with promise chain 
+
+// animatin with  async and await
+
+
+// alice1.animate(aliceTumbling, aliceTiming,);
