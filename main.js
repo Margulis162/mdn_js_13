61801cea2af45ec4d7d965012cfb99d0a1d6d6b6
Margulis162 <markmargulis@proton.me>
2023-10-01 15:21:57 -0400

    I have changed the image and animation a litle bit just for the fun. I might need to limit the number of iterations for the purpose of assignment. We shal see

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 const aliceTumbling = [
   { transform: 'rotate(0) scale(1)' },
-  { transform: 'rotate(360deg) scale(0)' }
+  { transform: 'rotate(360deg) scale(.7)' },
+  { transform: 'rotate(720deg) scale(1)' }
 ];
 
 const aliceTiming = {
   duration: 2000,
-  iterations: 1,
+  iterations: Infinity,
   fill: 'forwards'
 }
 
 const alice1 = document.querySelector("#alice1");
 const alice2 = document.querySelector("#alice2");
 const alice3 = document.querySelector("#alice3");
+
+
+alice1.animate(aliceTumbling, aliceTiming,);
