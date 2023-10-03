a68ac3a66ae587ff079e50c9257bae76ea89e0a4
Margulis162 <markmargulis@proton.me>
2023-10-02 20:58:56 -0400

    animation using promises

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const aliceTumbling = [
 ];
 
 const aliceTiming = {
-  duration: 1000000,
+  duration: 1000,
   iterations: 10,
   fill: 'forwards'
 }
@@ -22,27 +22,46 @@ const alice3 = document.querySelector("#alice3");
 
 // animating with pyramyd of dooom
 
-function animate_spiral(spiral, callback,){
-              // an alternative solution 
+// function animate_spiral(spiral, callback,){
+//               // an alternative solution 
 
-              // spiral.animate(aliceTumbling, aliceTiming).onfinish = callback;
-   let x = spiral.animate(aliceTumbling, aliceTiming,);
-   x.finished.then(() => {
-    callback();
-   }
+//               // spiral.animate(aliceTumbling, aliceTiming).onfinish = callback;
+//    let x = spiral.animate(aliceTumbling, aliceTiming,);
+//    x.finished.then(() => {
+//     callback();
+//    }
     
-   )
+//    )
    
-}
+// }
 
-animate_spiral( alice1, () => {
-  animate_spiral(alice2, () => {
-    animate_spiral(alice3,() => {
-      console.log("sucsess");
-    })
-  })
-})
+// animate_spiral( alice1, () => {
+//   animate_spiral(alice2, () => {
+//     animate_spiral(alice3,() => {
+//       console.log("sucsess");
+//     })
+//   })
+// })
+
 // animating with promise chain 
+
+// function animate_spiral(alice1){
+//   let x = alice1.animate(aliceTumbling, aliceTiming,);
+//   x.finished.then(animate_spiral(alice2)).then(animate_spiral(alice3));
+// }
+
+//   animate_spiral();
+
+  function animate_obj_fx(spiral){
+    let animate_obj = spiral.animate(aliceTumbling, aliceTiming);
+    return(animate_obj);
+  }
+
+  
+
+  animate_obj_fx(alice1).finished.then( () => animate_obj_fx(alice2).finished.then( () => animate_obj_fx(alice3)));
+  
+
 
 // animatin with  async and await
 
